1545428109d36013bfd0a6efb8c9e98300ea7b65
giangmt <maitruonggiangbkhn@gmail.com>
2018-03-19 17:08:50 +0700

    Update title in helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+	def full_title(page_title = '')
+		base_title = 'Hệ thống quản lý hiến máu'
+		if page_title.empty?
+			base_title
+		else
+			page_title + '|' + base_title
+		end
+	end
 end
